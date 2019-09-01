672424994b73f5f7c58c9ced012cb21015c25f50
Eugenio <eugenio.liso@gmail.com>
2019-09-01 21:53:46 +0200

    Working extraction script for HACS

--- datasets/HACS/extract_segments.py
+++ datasets/HACS/extract_segments.py
@@ -24,15 +24,35 @@ def hou_min_sec(millis):
     seconds = int(seconds)
     minutes = (millis / (1000 * 60)) % 60
     minutes = int(minutes)
-    hours = int(millis / (1000 * 60 * 60))
-    return "%d:%d:%d" % (hours, minutes, seconds)
+    hours = millis / (1000 * 60 * 60)
+
+    msecs = millis % 1000
+    return "%d:%d:%d.%d" % (hours, minutes, seconds, msecs)
+
+
+def _supermakedirs(path, mode):
+    if not path or os.path.exists(path):
+        return []
+    (head, _) = os.path.split(path)
+    res = _supermakedirs(head, mode)
+    os.mkdir(path)
+    os.chmod(path, mode)
+    res += [path]
+    return res
+
+
+def mkdir_p(path):
+    try:
+        _supermakedirs(path, 0o755)  # Supporting Python 2 & 3
+    except OSError:  # Python >2.5
+        pass
 
 
 if __name__ == '__main__':
     with open(annot_file, 'r') as loaded_annot_file:
         csvreader = csv.reader(loaded_annot_file, delimiter=',')
 
-        next(csvreader, None) # Skip headers
+        next(csvreader, None)  # Skip headers
 
         for annotation in csvreader:
             target_class = str(annotation[0]).replace(' ', '_')
@@ -45,22 +65,29 @@ if __name__ == '__main__':
                 end_time_seconds = float(annotation[4])
                 label = str(annotation[5])
 
-                if label == '1': # && subset = 'training' or subset = 'validation' FOR NOW I TAKE ALL THE VIDEOS AND DISCARD NEGATIVE EXAMPLES
+                if label == '1':  # && subset = 'training' or subset = 'validation' FOR NOW I TAKE ALL THE VIDEOS AND DISCARD NEGATIVE EXAMPLES
                     input_video = os.path.join(video_dir, target_class, "v_{}.mp4".format(video_id))
-                    output_video = os.path.join(output_dir, target_class, "v_{}_{}_{}.mp4".format(video_id,
-                                                                                                  start_time_seconds,
-                                                                                                  end_time_seconds))
 
-                    ffmpeg_command = 'rm %(outpath)s > /dev/null;  \
-                                          ffmpeg -ss %(start_timestamp)s -i \
-                                          %(videopath)s -g 1 -force_key_frames 0 \
-                                          -t %(clip_length)d -loglevel error %(outpath)s' % {
-                        'start_timestamp': hou_min_sec(start_time_seconds * 1000),
-                        # 'end_timestamp': hou_min_sec(clip_end * 1000),
-                        'clip_length': end_time_seconds,
-                        'videopath': input_video,
-                        'outpath': output_video}
+                    output_video_folder = os.path.join(output_dir, target_class)
+                    output_video = os.path.join(output_video_folder, "v_{}_{}_{}.mp4".format(video_id,
+                                                                                             start_time_seconds,
+                                                                                             end_time_seconds))
 
-                    subprocess.call(ffmpeg_command, shell=True)
+                    mkdir_p(output_video_folder)
+
+                    if os.path.exists(input_video):
+                        ffmpeg_command = 'rm %(outpath)s &> /dev/null; \
+                                              ffmpeg -ss %(start_timestamp)s -i \
+                                              %(videopath)s -g 1 -force_key_frames 0 \
+                                              -t %(clip_length)d -loglevel error %(outpath)s' % {
+                            'start_timestamp': hou_min_sec(start_time_seconds * 1000),
+                            # 'end_timestamp': hou_min_sec(clip_end * 1000),
+                            'clip_length': end_time_seconds,
+                            'videopath': input_video,
+                            'outpath': output_video}
+
+                        subprocess.call(ffmpeg_command, shell=True)
+                    else:
+                        print("The input video: {} does not exists".format(input_video))
 
     loaded_annot_file.close()
